8cbd506e79489840e028dcba7a5aa79b8166e634
besim <167048463+barryweb@users.noreply.github.com>
2025-04-08 21:41:04 +1000

    refactored & finalised the game

--- rps.js
+++ rps.js
@@ -33,11 +33,13 @@ function getHumanChoice() {
     }
 }
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
 
-function playRound(humanChoice, computerChoice) {
-    if ((humanChoice === rock && computerChoice === scissors) 
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        if ((humanChoice === rock && computerChoice === scissors) 
         || (humanChoice === paper && computerChoice === rock)
         || (humanChoice === scissors && computerChoice === paper)) {
             console.log(`Player choice: ${humanChoice} | Computer choice: ${computerChoice}`);
@@ -45,16 +47,46 @@ function playRound(humanChoice, computerChoice) {
             humanScore++;
             console.log(`Player score: ${humanScore} | Computer score: ${computerScore}`);
         } else if (humanChoice === computerChoice) {
-            console.log(`Player choice: ${humanChoice} | Computer choice: ${computerChoice}: It's a tie!`);
+            console.log(`Player choice: ${humanChoice} | Computer choice: ${computerChoice}`);
+            console.log("It's a tie!");
         } else {
-            console.log(`Computer choice: ${computerChoice} | Player choice: ${humanChoice}`);
+            console.log(`Player choice: ${humanChoice} | Computer choice: ${computerChoice}`);
             console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
             computerScore++;
-            console.log(`Computer score: ${computerScore} | Player score: ${humanScore}`);
+            console.log(`Player score: ${humanScore} | Computer score: ${computerScore}`);
         }
+    }
+
+    let askUser = 
+        prompt("How many rounds would you like to play? (Enter a number or enter 'exit' to quit):");
+
+    if (askUser === 'exit') return;
+
+    while (askUser !== 'exit' && (isNaN(askUser) || askUser <= 0)) {
+        if (askUser === 'exit') return;
+        askUser = prompt("Please enter a valid number of rounds (greather than 0):")
+    }
+
+    const numRounds = Number(askUser);
+
+    for (let i = 1; i <= numRounds; i++) {
+        console.log(`Round ${i}`);
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+
+        playRound(humanSelection, computerSelection);
+    }
+
+    if (humanScore === computerScore) {
+        console.log("It's a tie. Play another round!");
+        console.log(`Final scores: Player ${humanScore} | Computer ${computerScore}`);
+    } else if (humanScore > computerScore) {
+        console.log("Congratulations! You're the winner!");
+        console.log(`Final scores: Player ${humanScore} | Computer ${computerScore} `);
+    } else {
+        console.log("Computer wins the game!");
+        console.log(`Final scores: Player ${humanScore} | Computer ${computerScore} `);
+    }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playGame();
