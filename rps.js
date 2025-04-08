16441730ba02d3fe71638bd57efbcc1c7933bac4
besim <167048463+barryweb@users.noreply.github.com>
2025-04-08 19:00:55 +1000

    added function to play a single round

--- rps.js
+++ rps.js
@@ -35,3 +35,26 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if ((humanChoice === rock && computerChoice === scissors) 
+        || (humanChoice === paper && computerChoice === rock)
+        || (humanChoice === scissors && computerChoice === paper)) {
+            console.log(`Player choice: ${humanChoice} | Computer choice: ${computerChoice}`);
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+            humanScore++;
+            console.log(`Player score: ${humanScore} | Computer score: ${computerScore}`);
+        } else if (humanChoice === computerChoice) {
+            console.log(`Player choice: ${humanChoice} | Computer choice: ${computerChoice}: It's a tie!`);
+        } else {
+            console.log(`Computer choice: ${computerChoice} | Player choice: ${humanChoice}`);
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+            computerScore++;
+            console.log(`Computer score: ${computerScore} | Player score: ${humanScore}`);
+        }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
